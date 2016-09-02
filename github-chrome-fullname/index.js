2bfbbc7034777132da26b237e9b7a94194f83cf6
Frank Schippert <frank.schippert@sap.com>
2016-09-02 15:47:17 +0200

    Whitelist pages for userId replacement

--- github-chrome-fullname/index.js
+++ github-chrome-fullname/index.js
@@ -1,6 +1,11 @@
 (function() {
     "use strict";
 
+    var allowedSites = [
+        "github.wdf.sap.corp",
+        "huboard.mo.sap.corp"
+    ];
+
     /*global ReplaceRestricter, UserIdStringReplacer, UserIdReplacer*/
     var restricter = new ReplaceRestricter();
     var userIdStringReplacer = new UserIdStringReplacer("https://github.wdf.sap.corp");
@@ -8,6 +13,15 @@
 
     // Check DOM size every second. After change of DOM elements replace user Ids.
     var lastDomSize;
+
+    var parser = document.createElement('a');
+    parser.href = window.location.href;
+
+    var hostname = parser.hostname;
+    if (allowedSites.indexOf(hostname) === -1) {
+      return;
+    }
+
     window.setInterval(function() {
         if (!restricter.isAllowedUrl(window.location.href)) {
             return;
